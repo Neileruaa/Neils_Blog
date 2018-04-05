047c3648113d9a6f462081b8bb253699608c725f
Neilerua <aureliendrey@gmail.com>
2018-04-05 13:35:53 +0200

    Editor in Redactor.php and update admin.php

--- admin/Admin.php
+++ admin/Admin.php
@@ -3,47 +3,15 @@ require_once('../Connect_db.php');
 session_start();
 
 if ($_SESSION['isAdmin'] == true) {
-    echo "Welcome " . $_SESSION['authUser'];
-}else {
-    echo "Get out you're not authorized";
+    echo "Welcome " . $_SESSION['authUser'];?>
+
+    <br>
+    <hr>
+    <a href="Redactor.php">Rediger un article</a>
+<?php }else {
+    echo "Get out you're not authorized, Sign in ?";
+    echo "<a href='index.php'>Connect</a>";
+
 }
 
-
-?>
-
-<html>
-<head>
-    <meta charset="utf-8">
-    <title>Admin Article </title>
-</head>
-<body>
-<h1>Rédaction d'un article : </h1>
-<div>
-    <div>
-        <div>Choisissez la catégorie :</div>
-        <div>
-            <select>
-                <?php
-                foreach ($categories as $category)
-                {
-                    echo "<option value='".$category."'>".$category."</option>";
-                }
-                ?>
-            </select>
-        </div>
-        <div>
-            Ajouter une nouvelle catégorie :
-            <button id="addCategory">Créer une nouvelle catégorie</button>
-        </div>
-    </div>
-    <div><textarea name="editor"></textarea></div>
-</div>
-
-<script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
-<script src="//cdn.ckeditor.com/4.5.11/standard/ckeditor.js"></script>
-<script>
-    CKEDITOR.replace( 'editor' );
-</script>
-</body>
-</html>
 
--- admin/Redactor.php
+++ admin/Redactor.php
@@ -1,7 +1,42 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: aurel
- * Date: 04/04/18
- * Time: 22:16
- */
+
+require_once('../Connect_db.php');
+session_start();
+?>
+
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Admin Article </title>
+</head>
+<body>
+<h1>Rédaction d'un article : </h1>
+<div>
+    <div>
+        <div>Choisissez la catégorie :</div>
+        <div>
+            <select>
+                <?php
+                foreach ($categories as $category)
+                {
+                    echo "<option value='".$category."'>".$category."</option>";
+                }
+                ?>
+            </select>
+        </div>
+        <div>
+            Ajouter une nouvelle catégorie :
+            <button id="addCategory">Créer une nouvelle catégorie</button>
+        </div>
+    </div>
+    <div><textarea name="editor"></textarea></div>
+</div>
+
+<script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
+<script src="//cdn.ckeditor.com/4.5.11/standard/ckeditor.js"></script>
+<script>
+    CKEDITOR.replace( 'editor' );
+</script>
+</body>
+</html>
+
