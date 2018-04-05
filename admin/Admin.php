3892261bd292728b0c331edb0d21102b44252618
Neilerua <aureliendrey@gmail.com>
2018-04-05 11:18:54 +0200

    Add Editor to Admin.php

--- admin/Admin.php
+++ admin/Admin.php
@@ -7,3 +7,43 @@ if ($_SESSION['isAdmin'] == true) {
 }else {
     echo "Get out you're not authorized";
 }
+
+
+?>
+
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Admin Article </title>
+</head>
+<body>
+<h1>Rédaction d'un article : </h1>
+<div>
+    <div>
+        <div>Choisissez la catégorie :</div>
+        <div>
+            <select>
+                <?php
+                foreach ($categories as $category)
+                {
+                    echo "<option value='".$category."'>".$category."</option>";
+                }
+                ?>
+            </select>
+        </div>
+        <div>
+            Ajouter une nouvelle catégorie :
+            <button id="addCategory">Créer une nouvelle catégorie</button>
+        </div>
+    </div>
+    <div><textarea name="editor"></textarea></div>
+</div>
+
+<script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
+<script src="//cdn.ckeditor.com/4.5.11/standard/ckeditor.js"></script>
+<script>
+    CKEDITOR.replace( 'editor' );
+</script>
+</body>
+</html>
+
