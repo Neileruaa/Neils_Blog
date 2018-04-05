5816a69e518743f00874d3d2a807f16f9d9333ed
Aurel <aureliendrey@gmail.com>
2018-04-05 15:37:12 +0200

    Ajout catégorie

--- admin/Redactor.php
+++ admin/Redactor.php
@@ -1,7 +1,25 @@
 <?php
-
 require_once('../Connect_db.php');
 session_start();
+
+$categories = [];
+
+    if (isset($_POST["name"])){
+        $sql= "INSERT INTO CATEGORY(name) VALUES (:name);";
+        $stmt = $bdd->prepare($sql);
+        $stmt -> bindValue(':name', $_POST['name']);
+        $stmt->execute();
+    }
+$sql = "SELECT idCategory,name name FROM CATEGORY;";
+$stmt = $bdd -> prepare($sql);
+$stmt->execute();
+
+$categories = $stmt->fetchAll();
+
+if (isset($_POST["name"])){
+    echo json_encode($categories);
+    return;
+}
 ?>
 
 <html>
@@ -15,27 +33,46 @@ session_start();
     <div>
         <div>Choisissez la catégorie :</div>
         <div>
-            <select>
+            <select id="listCategories">
                 <?php
                 foreach ($categories as $category)
                 {
-                    echo "<option value='".$category."'>".$category."</option>";
+                    echo "<option value='".$category[0]."'>".$category["name"]."</option>";
                 }
                 ?>
             </select>
         </div>
         <div>
             Ajouter une nouvelle catégorie :
-            <button id="addCategory">Créer une nouvelle catégorie</button>
+            <button id="addCategory" >Créer une nouvelle catégorie</button>
         </div>
     </div>
+    <hr>
     <div><textarea name="editor"></textarea></div>
 </div>
 
 <script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
-<script src="//cdn.ckeditor.com/4.5.11/standard/ckeditor.js"></script>
+<script src="//cdn.ckeditor.com/4.9.1/full/ckeditor.js"></script>
 <script>
     CKEDITOR.replace( 'editor' );
+
+    $('#addCategory').on('click', function(){
+        var newName = prompt("Entrez le nom de votre catégorie :");
+        $.ajax({
+            method: "POST",
+            data: {
+                "name": newName
+            },
+            dataType: "json",
+            success: function(categories){
+                var categoriesHtml;
+                for (category of categories){
+                    categoriesHtml += "<option value='" + category[0] + "'>" + category["name"] +  "</option>";
+                }
+                $('#listCategories').html(categoriesHtml);
+            }
+        });
+    });
 </script>
 </body>
 </html>
